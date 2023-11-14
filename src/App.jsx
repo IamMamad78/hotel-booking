a52bb790575a38de2c80a11bfc4a886e3bd33fd5
IamMamad78 <Mamadi78.94@gmail.com>
2023-11-14 23:16:27 +0330

    LocationList added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Toaster } from "react-hot-toast";
 import "./App.css";
 import Header from "./components/Header/Header";
+import LocationLiast from "./components/LocationLiast/LocationLiast";
 
 function App() {
   return (
     <>
       <Toaster />
       <Header />
+      <LocationLiast />
     </>
   );
 }
