49348ba08f5f45675b52a7a4a7195f45bde95641
IamMamad78 <Mamadi78.94@gmail.com>
2023-11-15 00:53:54 +0330

    nested Route added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import { Route, Routes } from "react-router-dom";
 import LocationList from "./components/LocationLiast/LocationList";
+import AppLayout from "./components/AppLayout/AppLayout";
 
 function App() {
   return (
@@ -10,7 +11,11 @@ function App() {
       <Toaster />
       <Header />
       <Routes>
-        <Route path="/" element={<LocationList/>}/>
+        <Route path="/" element={<LocationList />} />
+        <Route path="/hotels" element={<AppLayout />}>
+          <Route index element={<div>Hotels</div>} />
+          <Route path=":id" element={<div>Single Hotel</div>} />
+        </Route>
       </Routes>
     </>
   );
