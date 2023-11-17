35ff5ff5a2525e6fb5d70226ca031196982d2467
IamMamad78 <Mamadi78.94@gmail.com>
2023-11-18 00:59:09 +0330

    useGeoLocation added

---
+++ src/hooks/useGeoLocation.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+export default function useGeoLocation() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [position, setPosition] = useState({});
+  const [error, setError] = useState(null);
+
+  function getLocation() {
+    if (!navigator.geolocation) {
+      return setError("Does Not Support By Your PC");
+    }
+
+    setIsLoading(true);
+    navigator.geolocation.getCurrentPosition(
+      (pos) => {
+        setPosition({
+          lat: pos.coords.latitude,
+          lng: pos.coords.longitude,
+        });
+        setIsLoading(false);
+      },
+      (error) => {
+        setError(error.message);
+        setIsLoading(false);
+      }
+    );
+  }
+  return { isLoading, error, position, getLocation };
+}
