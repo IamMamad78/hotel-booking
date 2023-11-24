31212edf7c3c88be443600483d3a607ea9716b9e
IamMamad78 <Mamadi78.94@gmail.com>
2023-11-24 12:07:00 +0330

    ProtectedRoute added

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import SingleBookamrk from "./components/SingleBookmark/SingleBookmark";
 import AddNewBookmark from "./components/AddNewBookmark/AddNewBookmark";
 import Login from "./components/Login/Login";
 import AuthProvider from "./context/AuthProvider";
+import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute";
 
 function App() {
   return (
@@ -29,7 +30,14 @@ function App() {
               <Route index element={<Hotels />} />
               <Route path=":id" element={<SingleHotel />} />
             </Route>
-            <Route path="/bookmark" element={<BookmarkLayout />}>
+            <Route
+              path="/bookmark"
+              element={
+                <ProtectedRoute>
+                  <BookmarkLayout />
+                </ProtectedRoute>
+              }
+            >
               <Route index element={<Bookmark />} />
               <Route path=":id" element={<SingleBookamrk />} />
               <Route path="add" element={<AddNewBookmark />} />
